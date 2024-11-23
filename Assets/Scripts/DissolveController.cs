f4e6295bdfe4fc580f2ff4080bf1e64cdac268de
Mirabell <elmirabegieva36@gmail.com>
2024-11-23 15:25:10 +0300

    added particle effects

--- Assets/Scripts/DissolveController.cs
+++ Assets/Scripts/DissolveController.cs
@@ -3,15 +3,22 @@ public class DissolveController : MonoBehaviour
 {
     public Renderer rend;
     public float dissolveSpeed = 1f;
+    public GameObject effect;
     private float _disolveAmount = 1f;
-    private bool dissolving = false;
+    private bool _dissolving = false;
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.D))
         {
-            dissolving = !dissolving;
+            _dissolving = !_dissolving;
+            
+            if (effect != null)
+            {
+                effect.SetActive(true);
+            }
         }
-        if (dissolving == true)
+        if (_dissolving == true)
         {
             _disolveAmount = Mathf.MoveTowards(_disolveAmount, 0f, dissolveSpeed * Time.deltaTime);
         }
@@ -20,5 +27,14 @@ public class DissolveController : MonoBehaviour
             _disolveAmount = Mathf.MoveTowards(_disolveAmount, 1f, dissolveSpeed * Time.deltaTime);
         }
         rend.material.SetFloat("_Cutoff", _disolveAmount);
+        
+        if (_disolveAmount <= .5f)
+        {
+            rend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        }
+        else
+        {
+            rend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        }
     }
 }
